821db5a522a16e1cbb5d95d3d52611b3bdd11837
chaeyeong222 <iky1505@naver.com>
2025-07-21 22:42:27 +0900

    :sparkles: Feat: 사업자 회원가입, 로그인 기능 구현

--- src/main/java/com/cy/rememeber/controller/UserController.java
+++
@@ -1,8 +0,0 @@
-package com.cy.rememeber.controller;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class UserController {
-
-}
---
+++ src/main/java/com/cy/rememeber/service/StoreService.java
@@ -0,0 +1,44 @@
+package com.cy.rememeber.service;
+
+import com.cy.rememeber.Entity.Store;
+import com.cy.rememeber.dto.StoreSignDto;
+import com.cy.rememeber.repository.StoreRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class StoreService {
+
+    private final PasswordEncoder passwordEncoder;
+    private final StoreRepository storeRepository;
+
+    /**
+    * 회원가입, STORE 생성
+    * */
+    public boolean signUp(StoreSignDto storeSignDto) throws Exception{
+        if(storeRepository.findById(storeSignDto.getId()).isPresent()){
+//            throw new Exception("이미 존재하는 아이디입니다.");
+            return false;
+        }
+
+        Store store = Store.builder()
+            .id(storeSignDto.getId())
+            .name(storeSignDto.getName())
+            .email(storeSignDto.getEmail())
+            .password(storeSignDto.getPassword())
+            .build();
+        store.passwordEncode(passwordEncoder);
+        storeRepository.save(store);
+        return true;
+    }
+
+    public Store getStore(String storeId){
+        return storeRepository.findById(storeId).orElse(null);
+    }
+
+}
