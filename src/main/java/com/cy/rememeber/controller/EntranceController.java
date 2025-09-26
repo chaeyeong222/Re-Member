22cfc475461eab135713e17b0e5059116ddb18b4
chaeyeong222 <iky1505@naver.com>
2025-09-26 15:17:33 +0900

    enter시 storekey 파라미터 추가, token 검사 후 return 값 변경

--- src/main/java/com/cy/rememeber/controller/EntranceController.java
+++ src/main/java/com/cy/rememeber/controller/EntranceController.java
@@ -8,10 +8,12 @@ import java.util.Arrays;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/enter")
+@RestController
+@RequestMapping("/enter")
 @RequiredArgsConstructor
 public class EntranceController {
     private final UserQueueService userQueueService;
@@ -20,11 +22,14 @@ public class EntranceController {
     public ResponseEntity<UserStatusResponseDto> checkStatus(
         @RequestParam(name = "queue", defaultValue = "default") String queue,
         @RequestParam(name = "user_id") Long userId,
+        @RequestParam(name = "store_key") String storeKey,
         HttpServletRequest request) {
 
+        System.out.println("get request :  userId "+ userId);
         var cookies = request.getCookies();
         var cookieName = "user-queue-%s-token".formatted(queue);
 
+        System.out.println(cookieName + "                 cookieName,  queuename");
         //쿠키 담겨있는 값들 중 큐이름에 해당하는 값을 가져옴
         String token = "";
         if (cookies != null) {
@@ -37,15 +42,18 @@ public class EntranceController {
         //토큰 검사
         boolean isAllowed = userQueueService.isAllowedByToken(queue, userId, token);
 
+        System.out.println("isallowed? "+ isAllowed);
         if (isAllowed) {
+            String bookingUrl = "/booking/" + storeKey;
             return ResponseEntity.ok(
-                new UserStatusResponseDto(true, "/reservations") //예약페이지로 이동
+                    new UserStatusResponseDto(true, bookingUrl)
             );
         } else {
-            String redirectUrl = "http://localhost:8090/waiting-room?user_id=%d&redirect_url=%s".formatted(
-                userId, "localhost:8090/reservations"); //입장가능해지면 이동할 페이지 s 에 들어감
+            String finalRedirectUrl = "http://localhost:3000/booking/" + storeKey;
+            String waitingRoomUrl = "http://localhost:3000/waiting-room?queue=%s&user_id=%d&redirect_url=%s".formatted(
+                    queue, userId, finalRedirectUrl);
             return ResponseEntity.ok(
-                new UserStatusResponseDto(false, redirectUrl)
+                    new UserStatusResponseDto(false, waitingRoomUrl)
             );
         }
     }
