b555b0e46d774ec633a6a885ebb60e946a11bf90
chaeyeong222 <iky1505@naver.com>
2025-11-28 23:26:18 +0900

    :recycle: Refactor: 조회용 메서드에 transacitonal readonly 적용

--- src/main/java/com/cy/rememeber/config/ReplicationRoutingDataSource.java
+++ src/main/java/com/cy/rememeber/config/ReplicationRoutingDataSource.java
@@ -1,4 +1,16 @@
 package com.cy.rememeber.config;
 
-public class ReplicationRoutingDataSource {
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+public class ReplicationRoutingDataSource extends AbstractRoutingDataSource {
+
+    @Override
+    protected Object determineCurrentLookupKey() {
+        // 현재 트랜잭션이 읽기 전용인지 확인
+        boolean isReadOnly = TransactionSynchronizationManager.isCurrentTransactionReadOnly();
+
+        // 읽기 전용이면 SLAVE, 아니면 MASTER 반환
+        return isReadOnly ? "SLAVE" : "MASTER";
+    }
 }
